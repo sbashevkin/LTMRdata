6f741e75b1005ef9343741cdc033baee10edf1e6
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-05-04 17:22:25 -0700

    Starting to document all code and adding Kathy's updated station locations for Baystudy

--- data-raw/Baystudy.R
+++ data-raw/Baystudy.R
@@ -8,14 +8,19 @@ require(lubridate)
 require(readxl)
 require(LTMRdata)
 
-stations_baystudy <- read_excel(file.path("data-raw", "Baystudy", "Bay Study_Station Coordinates for Distribution_2018.xlsx"))%>%
-  separate(Latitude, into=c("Lat_Deg", "Lat_Min"), sep = "°", convert=T)%>%
+# Station locations -------------------------------------------------------
+
+stations_baystudy <- read_excel(file.path("data-raw", "Baystudy", "Bay Study_Station Coordinates for Distribution_04May2020.xlsx"))%>%
+  separate(Latitude, into=c("Lat_Deg", "Lat_Min"), sep = "°", convert=T)%>% # Convert to decimal degrees
   separate(Longitude, into=c("Lon_Deg", "Lon_Min"), sep = "°", convert=T)%>%
   mutate(Latitude=Lat_Deg+Lat_Min/60,
          Longitude=Lon_Deg-Lon_Min/60)%>%
   select(Station, Latitude, Longitude)%>%
-  filter(Station!="211E")%>%
+  filter(Station!="211E")%>% # Kathy said W location of station 211 is more often used, so using those coordinates
   mutate(Station=recode(Station, `211W`="211"))
+
+
+# Import lookup tables ----------------------------------------------------
 
 tidecodes_baystudy <- read_csv(file.path("data-raw", "Baystudy", "TideCodes_LookUp.csv"),
                                col_types=cols_only(Tide="i", Description="c"))
@@ -26,11 +31,14 @@ wavecodes_baystudy <- read_csv(file.path("data-raw", "Baystudy", "WaveCodes_Look
 cloudcovercodes_baystudy <- read_csv(file.path("data-raw", "Baystudy", "CloudCover_LookUp.csv"),
                                col_types=cols_only(CloudCover="i", Description="c"))
 
+# Sample-level data -------------------------------------------------------
+
 salintemp_baystudy<-read_csv(file.path("data-raw", "Baystudy", "SalinTemp.csv"),
                              col_types = cols_only(Year="i", Survey="i", Station="c",
                                                    ECSurf="d", ECAvg="d", ECBott="d",
                                                    TempSurf="d", TempAvg="d", TempBott="d"))
 
+# Station-visit-level data
 
 boatstation_baystudy <- read_csv(file.path("data-raw", "Baystudy", "BoatStation.csv"),
                                  col_types = cols_only(Year="i", Survey="i", Station="c",
@@ -38,17 +46,19 @@ boatstation_baystudy <- read_csv(file.path("data-raw", "Baystudy", "BoatStation.
                                                        SubstrateCode="c", Waves="i", CloudCover="i",
                                                        Tide="i", StationComment="c"))%>%
   mutate(Date=parse_date_time(Date, orders="%m/%d/%Y %H:%M:%S", tz="America/Los_Angeles"))%>%
-  left_join(tidecodes_baystudy, by="Tide")%>%
+  left_join(tidecodes_baystudy, by="Tide")%>% # Convert tide codes to values
   select(-Tide, -SubstrateCode)%>%
   rename(Tidestation=Description)%>%
-  left_join(wavecodes_baystudy, by="Waves")%>%
+  left_join(wavecodes_baystudy, by="Waves")%>% # Convert wave codes to values
   select(-Waves)%>%
   rename(Waves=Description)%>%
-  left_join(cloudcovercodes_baystudy, by="CloudCover")%>%
+  left_join(cloudcovercodes_baystudy, by="CloudCover")%>% # Convert cloud cover codes to values
   select(-CloudCover)%>%
   rename(CloudCover=Description)%>%
-  left_join(salintemp_baystudy, by=c("Year", "Survey", "Station"))%>%
+  left_join(salintemp_baystudy, by=c("Year", "Survey", "Station"))%>% #Add salinity and temperature data
   left_join(stations_baystudy, by="Station")
+
+# Tow-level data
 
 boattow_baystudy<-read_csv(file.path("data-raw", "Baystudy", "BoatTow.csv"),
                            col_types = cols_only(Year="i", Survey="i", Station="c", Net="i",
@@ -59,24 +69,29 @@ boattow_baystudy<-read_csv(file.path("data-raw", "Baystudy", "BoatTow.csv"),
                                                  EndLat="d", Distance="d", TowComment="c"))%>%
   select(-StartLong, -EndLong, -StartLat, -EndLat, -StartMeter, -EndMeter)%>% # Removing survey lats/longs and start/end meters for now
   mutate(Time=parse_date_time(Time, orders="%m/%d/%Y %H:%M:%S"),
-         Tow_direction=recode(Direction, `1`="With current", `2`="Against current", `3`="Slack or cross-current"),
-         Tow_volume=TotalMeter * 0.02687 * 10.7,
-         Tow_area=Distance*3.42)%>%
-  left_join(tidecodes_baystudy, by="Tide")%>%
+         Tow_direction=recode(Direction, `1`="With current", `2`="Against current", `3`="Slack or cross-current"), # Convert tow direction codes to values
+         Tow_volume=TotalMeter * 0.02687 * 10.7, # Calculate tow volume using formulas in database metadata
+         Tow_area=Distance*3.42)%>% # Calculate tow area using formulas in database metadata
+  left_join(tidecodes_baystudy, by="Tide")%>% # Convert tide codes to values
   select(-Tide, -Direction)%>%
   rename(Method=Net, Tidetow=Description)%>%
-  mutate(Method=recode(Method, `1`="Midwater trawl", `2`="Otter trawl", `3`="EL"))%>%
-  filter(Method%in%c("Midwater trawl", "Otter trawl"))%>%
-  mutate(TowStatus=recode(Tow, `0`="Invalid", `1`="Valid", `2`="Valid", `51`="Valid", `52`="Invalid", `53`="Invalid", `54`="Valid", `55`="Valid", `56`="Valid", `57`="Valid", `58`="Invalid"))%>%
-  select(-Tow, -TotalMeter, -Distance)
+  mutate(Method=recode(Method, `1`="Midwater trawl", `2`="Otter trawl", `3`="EL"))%>% # Convert method codes to values
+  filter(Method%in%c("Midwater trawl", "Otter trawl"))%>% # Only include midwater and otter trawls
+  mutate(TowStatus=recode(Tow, `0`="Invalid", `1`="Valid", `2`="Valid", `51`="Valid", `52`="Invalid",
+                          `53`="Invalid", `54`="Valid", `55`="Valid", `56`="Valid", `57`="Valid", `58`="Invalid"))%>% # Classify TowStatus into Valid or Invalid tows
+  select(-Tow, -TotalMeter, -Distance) # Remove unneeded variables
 
-env_baystudy <- left_join(boattow_baystudy, boatstation_baystudy, by=c("Year", "Survey", "Station"))%>%
-  mutate(Tide=if_else(is.na(Tidetow), Tidestation, Tidetow),
+# All sample-level data
+
+env_baystudy <- left_join(boattow_baystudy, boatstation_baystudy, by=c("Year", "Survey", "Station"))%>% # Join together station-visit and tow - level data
+  mutate(Tide=if_else(is.na(Tidetow), Tidestation, Tidetow), # Tide was sometimes recorded at each station visit and sometimes at each tow
          Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste0(Date, " ", hour(Time), ":", minute(Time))), "%Y-%m-%d %%H:%M", tz="America/Los_Angeles"),
-         SampleID=1:nrow(.))%>%
-  select(-Tidestation, -Tidetow, -Time)
+         SampleID=1:nrow(.))%>% # Create identifier for each sample (tow)
+  select(-Tidestation, -Tidetow, -Time) # Remove unneeded variables
 
-rm(tidecodes_baystudy, wavecodes_baystudy, cloudcovercodes_baystudy, boattow_baystudy, boatstation_baystudy, salintemp_baystudy, stations_baystudy)
+rm(tidecodes_baystudy, wavecodes_baystudy, cloudcovercodes_baystudy, boattow_baystudy, boatstation_baystudy, salintemp_baystudy, stations_baystudy) # Clean up
+
+# Catch data --------------------------------------------------------------
 
 catch_baystudy <- read_csv(file.path("data-raw", "Baystudy", "Fish Catch Data.csv"),
                            col_types=cols_only(Year="i", Survey="i", Station="c",
@@ -84,70 +99,78 @@ catch_baystudy <- read_csv(file.path("data-raw", "Baystudy", "Fish Catch Data.cs
                                                SizeGroup="i", QtsCaught="d", QtsSubsampled="d",
                                                PlusCount="d"))%>%
   rename(Method=Net)%>%
-  mutate(Method=recode(Method, `1`="Midwater trawl", `2`="Otter trawl", `3`="EL"))%>%
-  filter(Method%in%c("Midwater trawl", "Otter trawl"))%>%
-  left_join(Species%>%
+  mutate(Method=recode(Method, `1`="Midwater trawl", `2`="Otter trawl", `3`="EL"))%>% # Convert method codes to values
+  filter(Method%in%c("Midwater trawl", "Otter trawl"))%>% #Only keep midwater and otter trawls
+  left_join(Species%>% # Add species names
               select(AlphaCode=Baystudy_Code, Taxa)%>%
               filter(!is.na(AlphaCode)),
             by="AlphaCode")%>%
-  select(-AlphaCode)
+  select(-AlphaCode) # Remove unneeded variable
+
+# Length data -------------------------------------------------------------
 
 length_baystudy <- read_csv(file.path("data-raw", "Baystudy", "Fish Length Data.csv"),
                             col_types=cols_only(Year="i", Survey="i", Station="c",
                                                 Net="i", AlphaCode="c",
                                                 SizeGroup="i", Length="d", Frequency="d"))%>%
   rename(Method=Net)%>%
-  mutate(Method=recode(Method, `1`="Midwater trawl", `2`="Otter trawl", `3`="EL"),
-         AlphaCode = toupper(AlphaCode))%>%
-  filter(Method%in%c("Midwater trawl", "Otter trawl"))%>%
-  left_join(Species%>%
+  mutate(Method=recode(Method, `1`="Midwater trawl", `2`="Otter trawl", `3`="EL"), # Convert method codes to values
+         AlphaCode = toupper(AlphaCode))%>% # Make alphacodes consistent
+  filter(Method%in%c("Midwater trawl", "Otter trawl"))%>% #Only keep midwater and otter trawls
+  left_join(Species%>% # Add species names
               select(AlphaCode=Baystudy_Code, Taxa)%>%
               filter(!is.na(AlphaCode)),
             by="AlphaCode")%>%
-  select(-AlphaCode)
+  select(-AlphaCode) # Remove unneeded variable
+
+# Join length and catch data
 
 lengthcatch_baystudy<-catch_baystudy%>%
-  full_join(length_baystudy%>%
+  full_join(length_baystudy%>% # This first join will just include total number measured
                group_by(Year, Survey, Station, Method, SizeGroup, Taxa)%>%
-               summarize(TotalMeasured=sum(Frequency))%>%
+               summarize(TotalMeasured=sum(Frequency))%>% # Calculate total number of each species of fish measured in each sample
                ungroup(),
              by=c("Year", "Survey", "Station", "Method", "SizeGroup", "Taxa"))%>%
-  filter(!is.na(TotalMeasured) & !is.na(PlusCount))%>%
-  mutate(TotalCatch=(TotalMeasured+PlusCount)*(QtsCaught/QtsSubsampled))%>%
-  select(-PlusCount, -QtsCaught, -QtsSubsampled)%>%
-  right_join(length_baystudy, by=c("Year", "Survey", "Station", "Method", "SizeGroup", "Taxa"))%>%
-  mutate(Count = (Frequency/TotalMeasured)*TotalCatch)%>%
-  select(-TotalMeasured, -TotalCatch, -Frequency)
+  filter(!is.na(TotalMeasured) & !is.na(PlusCount))%>% # Remove any rows corresponding to 0 fish measured and 0 fish counted
+  mutate(TotalCatch=(TotalMeasured+PlusCount)*(QtsCaught/QtsSubsampled))%>% # Calculate total number of each species caught in each sample
+  select(-PlusCount, -QtsCaught, -QtsSubsampled)%>% # Remove unneeded variables
+  right_join(length_baystudy, by=c("Year", "Survey", "Station", "Method", "SizeGroup", "Taxa"))%>% # Now join all measurements
+  mutate(Count = (Frequency/TotalMeasured)*TotalCatch)%>% # Calculate adjusted size frequency
+  select(-TotalMeasured, -TotalCatch, -Frequency) # Remove unneeded variables
 
-Baystudy <- env_baystudy%>%
-  left_join(lengthcatch_baystudy, by=c("Year", "Survey", "Station", "Method"))%>%
-  mutate(Sal_surf=ec2pss(ECSurf/1000, t=25),
+# Create final datasets ---------------------------------------------------
+
+Baystudy <- env_baystudy%>% # Start with sample-level data to retain samples with no catch (nets empty of fish)
+  left_join(lengthcatch_baystudy, by=c("Year", "Survey", "Station", "Method"))%>% # Add length and catch data
+  mutate(Sal_surf=ec2pss(ECSurf/1000, t=25), # Calculate salinity from conductivity
          Sal_avg=ec2pss(ECAvg/1000, t=25),
          Sal_bott=ec2pss(ECBott/1000, t=25),
          Source="Bay Study",
-         SampleID=paste(Source, SampleID),
-         Length_NA_flag=if_else(is.na(Length), "No fish caught", NA_character_))%>%
+         SampleID=paste(Source, SampleID), # Add unique identifier for each sample across all studies
+         Length_NA_flag=if_else(is.na(Length), "No fish caught", NA_character_))%>% # Add reasoning for an NA lengths (all "No Fish Caught" for Baystudy)
   select(-ECSurf, -ECAvg, -ECBott, -CatchCode, -SizeGroup, -StationComment)%>% # Remove unneeded variables
   group_by_at(vars(-Count))%>%
-  summarise(Count=sum(Count))%>%
+  summarise(Count=sum(Count))%>% # Add up counts when the same species and length were recorded multiple times for a sample
   ungroup()%>%
   rename(Notes_tow=TowComment, Tow_status=TowStatus,
          Weather=CloudCover, Temp_surf=TempSurf, Temp_avg=TempAvg,
          Temp_bott=TempBott, Tow_duration=Duration)%>%
   select(-Bearing, -Waves, -Weather, -Temp_avg, -Temp_bott, -Sal_avg, -Sal_bott)%>% # Remove extra environmental variables
   filter(Tow_status=="Valid")%>% # Remove invalid tows
-  select(Source, Station, Latitude, Longitude, Date, Datetime, Year, Survey,
+  select(Source, Station, Latitude, Longitude, Date, Datetime, Year, Survey, # Reorder variables for consistency
          Depth, SampleID, Method, Tide, Sal_surf, Temp_surf, Secchi,
          Tow_duration, Tow_area, Tow_volume, Tow_direction, Taxa, Length, Count, Length_NA_flag, Notes_tow)
 
+# Just measured lengths
+
 Baystudy_measured_lengths<-length_baystudy%>%
   inner_join(Baystudy%>% # Inner join to remove invalid tows
-              select(SampleID, Year, Survey, Station, Method)%>%
+              select(SampleID, Year, Survey, Station, Method)%>% # Add SampleID and Method to length data
               distinct(),
             by=c("Year", "Survey", "Station", "Method"))%>%
-  select(SampleID, Taxa, Size_group=SizeGroup, Length, Count=Frequency)
+  select(SampleID, Taxa, Size_group=SizeGroup, Length, Count=Frequency)# Reorder variables for consistency
 
 Baystudy <- Baystudy%>%
-  select(-Year, -Survey)
+  select(-Year, -Survey)# Remove unneeded variables
 
-usethis::use_data(Baystudy, Baystudy_measured_lengths, overwrite = TRUE)
+usethis::use_data(Baystudy, Baystudy_measured_lengths, overwrite = TRUE) # Save compressed data to /data
--- data-raw/FMWT.R
+++ data-raw/FMWT.R
@@ -6,6 +6,9 @@ require(tidyr)
 require(lubridate)
 require(LTMRdata)
 
+
+# Station locations -------------------------------------------------------
+
 stations_fmwt <- read_csv(file.path("data-raw", "FMWT", "StationsLookUp.csv"),
                           col_types = cols_only(StationCode="c", Active="i",
                                                 Lat="c", Long="c", Comments="c",
@@ -13,16 +16,21 @@ stations_fmwt <- read_csv(file.path("data-raw", "FMWT", "StationsLookUp.csv"),
   select(Station=StationCode, Active, Lat, Long, Station_notes=Comments, Lat2=`WGS84 Lat`, Long2=`WGS84 Long`)%>%
   separate(Lat, into=c("Lat_d", "Lat_m", "Lat_s"), sep="[ ]{1,}", convert=T)%>% # parse latitude and longitude into something usable
   separate(Long, into=c("Long_d", "Long_m", "Long_s"), sep="[ ]{1,}", convert=T)%>%
-  mutate(Latitude=Lat_d+Lat_m/60+Lat_s/3600,
+  mutate(Latitude=Lat_d+Lat_m/60+Lat_s/3600, # Convert Latitude and Longitude to decimal degrees
          Longitude=Long_d-Long_m/60-Long_s/3600)%>%
-  mutate(Latitude=if_else(is.na(Latitude), Lat2, Latitude),
-         Longitude=if_else(is.na(Longitude), -1*Long2, Longitude))%>%
+  mutate(Latitude=if_else(is.na(Latitude), Lat2, Latitude), #Use WGS84 Lats and Longs if values from Lat or Long columns are missing
+         Longitude=if_else(is.na(Longitude), -1*Long2, Longitude))%>% # WGS84 Longitude is positive
   select(Station, Latitude, Longitude, Active)%>%
   drop_na(Station, Latitude, Longitude) # Drop any rows with NAs in these variables
+
+
+# Sample dates ------------------------------------------------------------
 
 date_fmwt <- read_csv(file.path("data-raw", "FMWT", "Date.csv"), col_types=cols_only(DateID="i", SampleDate="c"))%>%
   mutate(SampleDate=parse_date_time(SampleDate, "%m/%d/%Y %H:%M:%S", tz="America/Los_Angeles"))%>%
   rename(Date=SampleDate)
+
+# Sample-level data -------------------------------------------------------
 
 sample_fmwt <- read_csv(file.path("data-raw", "FMWT", "Sample.csv"),
                         col_types = cols_only(SampleRowID="i", StationCode="c", MethodCode="c",
@@ -35,30 +43,35 @@ sample_fmwt <- read_csv(file.path("data-raw", "FMWT", "Sample.csv"),
   left_join(date_fmwt, by="DateID")%>% # Add dates
   rename(Station=StationCode, Method=MethodCode, Tide=TideCode, Time=SampleTimeStart, Depth=DepthBottom)%>%
   mutate(Time = parse_date_time(Time, "%m/%d/%Y %H:%M:%S", tz="America/Los_Angeles"),
-         Tide=recode(Tide, `1` = "High Slack", `2` = "Ebb", `3` = "Low Slack", `4` = "Flood"),
-         Weather=recode(WeatherCode, `1` = "Cloud (0-33%)", `2` = "Cloud (33-66%)", `3` = "Cloud (66-100%)", `4` = "Rain"),
-         Waves=recode(WaveCode, `1` = "Calm", `2` = "Waves w/o whitecaps", `3` = "Waves w/ whitecaps"),
-         WindDirection=toupper(WindDirection),
+         Tide=recode(Tide, `1` = "High Slack", `2` = "Ebb", `3` = "Low Slack", `4` = "Flood"), # Convert tide codes to values
+         Weather=recode(WeatherCode, `1` = "Cloud (0-33%)", `2` = "Cloud (33-66%)", `3` = "Cloud (66-100%)", `4` = "Rain"), # Convert weather codes to values
+         Waves=recode(WaveCode, `1` = "Calm", `2` = "Waves w/o whitecaps", `3` = "Waves w/ whitecaps"), # Convert wave codes to values
+         WindDirection=toupper(WindDirection), # Make Wind direction codes consistent
          Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste0(Date, " ", hour(Time), ":", minute(Time))), "%Y-%m-%d %%H:%M", tz="America/Los_Angeles"),
-         Meter_total=MeterEnd-MeterStart)%>%
-  mutate(Tow_volume = Meter_total*0.02687*10.7,
-         WindDirection=recode(WindDirection, "NA"=NA_character_, "N/A"=NA_character_))%>%
-  filter(Method=="MWTR")%>% # All rows are MWTR but just in case the data change
-  mutate(Method=recode(Method, MWTR="Midwater trawl"),
-         Microcystis=recode(Microcystis, `1`="Absent", `2`="Low", `6`="Low", `3`="Medium", `4`="High", `5`="Very high"),
-         Tow_direction = recode(TowDirectionCode, `1`="With current", `2`="Against current", `3`="Unknown"))%>%
-  select(-TowDirectionCode, -WeatherCode, -WaveCode, -MeterEnd, -MeterStart, -Meter_total, -DateID)%>%
-  left_join(stations_fmwt, by="Station")%>%
-  mutate(SampleID=1:nrow(.))
+         Meter_total=MeterEnd-MeterStart)%>% # Calculate flowmeter total difference
+  mutate(Tow_volume = Meter_total*0.02687*10.7, # Calculate tow volume using formula provided by Steve Slater / James White
+         WindDirection=recode(WindDirection, "NA"=NA_character_, "N/A"=NA_character_))%>% # Convert NA codes to NA
+  filter(Method=="MWTR")%>% # Select only Midwater Trawl. All rows are MWTR but just in case the data change
+  mutate(Method=recode(Method, MWTR="Midwater trawl"), # Recode method for consistency
+         Microcystis=recode(Microcystis, `1`="Absent", `2`="Low", `6`="Low", `3`="Medium", `4`="High", `5`="Very high"), #Convert Microcystis codes to values
+         Tow_direction = recode(TowDirectionCode, `1`="With current", `2`="Against current", `3`="Unknown"))%>% # Convert tow direction codes to values
+  select(-TowDirectionCode, -WeatherCode, -WaveCode, -MeterEnd, -MeterStart, -Meter_total, -DateID)%>% # Remove unneeded variables
+  left_join(stations_fmwt, by="Station")%>% # Add station coordinates
+  mutate(SampleID=1:nrow(.)) # Add unique identifier for each sample (net tow)
+
+# Catch data --------------------------------------------------------------
 
 catch_fmwt <- read_csv(file.path("data-raw", "FMWT", "Catch.csv"),
                        col_types = cols_only(CatchRowID="i", SampleRowID="i", OrganismCode="i", Catch="d"))%>%
-  filter(!is.na(OrganismCode))%>%
-  left_join(Species%>%
+  filter(!is.na(OrganismCode))%>% # Remove any records with an NA organism code
+  left_join(Species%>% # Add species names
               select(OrganismCode=FMWT_Code, Taxa)%>%
               filter(!is.na(OrganismCode)),
             by="OrganismCode")%>%
-  select(-OrganismCode)
+  select(-OrganismCode) # Remove unneeded variable
+
+
+# Length data -------------------------------------------------------------
 
 length_fmwt<- read_csv(file.path("data-raw", "FMWT", "Length.csv"), na=c("NA", "n/p"),
                        col_types = cols_only(CatchRowID="i", ForkLength="d", Dead="c", LengthFrequency="d"))%>%
@@ -66,40 +79,43 @@ length_fmwt<- read_csv(file.path("data-raw", "FMWT", "Length.csv"), na=c("NA", "
 
 catchlength_fmwt <- length_fmwt%>%
   group_by(CatchRowID)%>%
-  mutate(TotalMeasured=sum(LengthFrequency, na.rm=T))%>%
+  mutate(TotalMeasured=sum(LengthFrequency, na.rm=T))%>% # Calculate total number of fish measured for each species in each sample
   ungroup()%>%
-  left_join(catch_fmwt, by="CatchRowID")%>%
-  mutate(Count = (LengthFrequency/TotalMeasured)*Catch)
+  left_join(catch_fmwt, by="CatchRowID")%>% # Add catch numbers and species names
+  mutate(Count = (LengthFrequency/TotalMeasured)*Catch) # Calculate adjusted count
 
-FMWT<-sample_fmwt%>%
-  left_join(catchlength_fmwt, by="SampleRowID")%>%
-  mutate(Sal_surf=ec2pss(ConductivityTop/1000, t=25),
+# Create final datasets ---------------------------------------------------
+
+FMWT<-sample_fmwt%>% # Start with sample to ensure samples without any catch (empty nets) are included
+  left_join(catchlength_fmwt, by="SampleRowID")%>% # Join to catch/length data
+  mutate(Sal_surf=ec2pss(ConductivityTop/1000, t=25), # Convert conductivity to salinity
          Sal_bott=ec2pss(ConductivityBottom/1000, t=25),
          Secchi=Secchi*100, # Convert Secchi to cm from m
          Depth = Depth*0.3048, #Convert depth to m from feet
          Source="FMWT",
-         SampleID=paste(Source, SampleID),
-         Length_NA_flag=if_else(is.na(ForkLength), "No fish caught", NA_character_))%>%
+         SampleID=paste(Source, SampleID), # Add variable for unique (across all studies) sampleID
+         Length_NA_flag=if_else(is.na(ForkLength), "No fish caught", NA_character_))%>% # Add reasoning for an NA lengths (all "No Fish Caught" for FMWT)
   rename(Length=ForkLength, Temp_surf=WaterTemperature, Temp_bott=BottomTemperature,
          Secchi_estimated=SecchiEstimated, Survey=SurveyNumber, Cable_length=CableOut,
          Wind_direction=WindDirection)%>%
   select(-ConductivityTop, -ConductivityBottom, -LengthFrequency, -TotalMeasured,
-         -SampleRowID, -Time, -Catch, -Active, -Dead)%>%
+         -SampleRowID, -Time, -Catch, -Active, -Dead)%>% # Remove extra variables
   select(-Turbidity, -Microcystis, -Wind_direction, -Temp_bott, -Weather, -Waves, -Sal_bott)%>% # Remove extra environmental variables
-  select(Source, Station, Latitude, Longitude, Date, Datetime,
+  select(Source, Station, Latitude, Longitude, Date, Datetime, # Reorder variables for consistency
          Depth, SampleID, CatchRowID, Method, Tide, Sal_surf, Temp_surf, Secchi, Secchi_estimated,
          Tow_volume, Tow_direction, Cable_length, Taxa, Length, Count, Length_NA_flag)
 
+# Just measured lengths
 FMWT_measured_lengths<-length_fmwt%>%
-  left_join(FMWT%>%
+  left_join(FMWT%>% # Join species names and sampleID
               select(CatchRowID, SampleID, Taxa)%>%
               distinct(),
             by="CatchRowID")%>%
-  select(SampleID, Taxa, Dead, Length=ForkLength, Count=LengthFrequency)
+  select(SampleID, Taxa, Dead, Length=ForkLength, Count=LengthFrequency) # Reorder variables for consistency
 
 FMWT<-FMWT%>%
-  select(-CatchRowID)
+  select(-CatchRowID) # Remove unneeded variable
 
-rm(catchlength_fmwt, catch_fmwt, length_fmwt, sample_fmwt, date_fmwt, stations_fmwt)
+rm(catchlength_fmwt, catch_fmwt, length_fmwt, sample_fmwt, date_fmwt, stations_fmwt) # Clean up
 
-usethis::use_data(FMWT, FMWT_measured_lengths, overwrite=TRUE)
+usethis::use_data(FMWT, FMWT_measured_lengths, overwrite=TRUE) # Save compressed data to /data
