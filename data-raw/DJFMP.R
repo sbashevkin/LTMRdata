2e00e2a3b0745dc782b612cb2c350afc51d4a987
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2021-10-08 11:42:59 -0700

    Fixing DJFMP data

--- data-raw/DJFMP.R
+++ data-raw/DJFMP.R
@@ -1,7 +1,3 @@
-###################################################################
-## code to prepare `DJFMP` dataset as prepared by Sam Bashevkin  ##
-###################################################################
-
 require(wql)
 require(LTMRdata)
 require(readr)
@@ -32,19 +28,19 @@ DJFMP <-  bind_rows(
            col_types = cols_only(StationCode = "c", SampleDate = "c", SampleTime = "c",
                                  TowNumber = "c", MethodCode = "c",
                                  Conductivity = "d", WaterTemperature = "d", Secchi = "d",
-                                 Volume = "d", TowDirectionCode = "c",
+                                 Volume = "d", TowDirectionCode = "c", MarkCode="c", RaceByLength="c",
                                  OrganismCode = "c", ForkLength = "d", Count = "d")),
   read_csv(file.path(tempdir(), "DJFMP_2002-2020.csv"),
            col_types = cols_only(StationCode = "c", SampleDate = "c", SampleTime = "c",
                                  TowNumber = "c", MethodCode = "c",
                                  Conductivity = "d", WaterTemperature = "d", Secchi = "d",
-                                 Volume = "d", TowDirectionCode = "c",
+                                 Volume = "d", TowDirectionCode = "c", MarkCode="c", RaceByLength="c",
                                  OrganismCode = "c", ForkLength = "d", Count = "d")),
   read_csv(file.path(tempdir(), "DJFMP_BeachSeine_1976-2020.csv"),
            col_types = cols_only(StationCode = "c", SampleDate = "c", SampleTime = "c",
                                  TowNumber = "c", MethodCode = "c", SeineDepth = "d",
                                  Conductivity = "d", WaterTemperature = "d", Secchi = "d",
-                                 Volume = "d", TowDirectionCode = "c",
+                                 Volume = "d", TowDirectionCode = "c", MarkCode="c", RaceByLength="c",
                                  OrganismCode = "c", ForkLength = "d", Count = "d")))%>%
   rename(Station = StationCode, Date = SampleDate, Time = SampleTime, Temp_surf = WaterTemperature,
          Method = MethodCode, Tow_volume = Volume, Depth=SeineDepth,
@@ -60,16 +56,26 @@ DJFMP <-  bind_rows(
          Sal_surf = ec2pss(Conductivity/1000, t=25),
          Method = recode(Method, MWTR="Midwater trawl", KDTR="Kodiak trawl", SEIN="Beach seine"),
          Tow_direction = recode(Tow_direction, U="Upstream", D="Downstream", X="Neither"),
-         SampleID=paste(Datetime, Station, TowNumber, Method)) %>%
-  select(-Time) %>%
+         SampleID=paste(Datetime, Station, TowNumber, Method),
+         SalmonCode=if_else(MarkCode=="None" & OrganismCode=="CHN", RaceByLength, NA_character_))%>% # Set up code for Chinoook Salmon to ensure adjusted length frequency calculation works correctly
+  select(-Time, -MarkCode, -RaceByLength) %>%
   group_by(across(-Count))%>% # Some species are recorded with the same length multiple times
   summarise(Count=sum(Count), .groups="drop")%>%
-  group_by(SampleID, OrganismCode)%>%
-  mutate(TotalMeasured=sum(Count[which(Length!=0)]),
-         Total=sum(Count),
-         Count=(Count/TotalMeasured)*Total)%>%
+  group_by(SampleID, OrganismCode, SalmonCode)%>%
+  mutate(TotalMeasured=sum(Count[which(Length!=0)]), # Calculate total number of fish of each species measured
+         Total=sum(Count), # Calculate total number of fish of each species caught
+         Count=(Count/TotalMeasured)*Total)%>% # Calculate the adjusted length frequency
   ungroup()%>%
+  mutate(Length=if_else(is.infinite(Count) & Length==0, NA_real_, Length), # Some Chinook were not measured, so these lines fix some after-effects of that
+         Length_NA_flag=case_when(
+           is.infinite(Count) ~ "Unknown length",
+           is.na(Length)~ "No fish caught",
+           TRUE ~ NA_character_), # Add reasoning for an NA lengths (all "No Fish Caught" for FMWT)
+    Count=if_else(is.infinite(Count), Total, Count))%>%
   filter(Length!=0 | is.na(Length))%>%
+  select(-Total, -TotalMeasured, -SalmonCode)%>%
+  group_by(across(-Count))%>% # Add up any new multiples after removing SalmonCode
+  summarise(Count=sum(Count), .groups="drop")%>%
   left_join(DJFMP_stations, by = "Station") %>%
   # Add species names
   left_join(Species %>%
@@ -78,47 +84,9 @@ DJFMP <-  bind_rows(
   # Remove unneeded variable
   select(-OrganismCode) %>%
   mutate(SampleID=paste(Source, SampleID), # Add variable for unique (across all studies) sampleID
-         Length_NA_flag=if_else(is.na(Length), "No fish caught", NA_character_), # Add reasoning for an NA lengths (all "No Fish Caught" for FMWT)
          Taxa=str_remove(Taxa, " \\((.*)"))%>% # Remove life stage info from Taxa names
   select(Source, Station, Latitude, Longitude, Date, Datetime, Depth, SampleID, Method, Sal_surf,
          Temp_surf, Secchi, Tow_volume, Tow_direction, Taxa, Length, Count, Length_NA_flag)
 
 # Save compressed data to /data
 usethis::use_data(DJFMP, overwrite=TRUE, compress="xz")
-
-#x = DJFMP[(DJFMP$Taxa == "Lucania goodei"),]
-
-# this line of code was used to test if all fish that had an "organism code" in the DJFMP data
-# had a Scientific Name of a fish associated with them. The only NA's in "Taxa" field are when
-# there was "no catch" which has the organism code: "NOFISH"
-# command below came up with ~35,000 entries; command below that with 0
-# djfmp.sppnas = DJFMP[is.na(DJFMP$Taxa),]
-# djfmp.sppnas = DJFMP[(is.na(DJFMP$Taxa) & DJFMP$OrganismCode != "NOFISH"),]
-# There is no instance where Count exists but Length does not - where is the PLUS COUNT ?
-# djfmp.test = DJFMP[(!is.na(DJFMP$Count) & is.na(DJFMP$Length)),]
-
-# DJFMP BEACH SEINE DATA ###########
-
-
-# # testing - ditch later
-# DJFMP2 <- read_csv(file.path(tempdir(), "DJFMP_1976-2001.csv"),
-#                   col_types = cols_only(StationCode = "c", SampleDate = "c", SampleTime = "c", flowDebris = "c",
-#                                         TowNumber = "c", MethodCode = "c", StartMeter = "d", EndMeter = "d", TotalMeter = "d",
-#                                         Conductivity = "d", WaterTemperature = "d", Secchi = "d",
-#                                         TowDuration = "d", Volume = "d", TowDirectionCode = "c",
-#                                         OrganismCode = "c", ForkLength = "d", Count = "d")) %>%
-#   bind_rows(read_csv(file.path(tempdir(), "DJFMP_2002-2020.csv"),
-#                      col_types = cols_only(StationCode = "c", SampleDate = "c", SampleTime = "c", flowDebris = "c",
-#                                            TowNumber = "c", MethodCode = "c", StartMeter = "d", EndMeter = "d", TotalMeter = "d",
-#                                            Conductivity = "d", WaterTemperature = "d", Secchi = "d",
-#                                            TowDuration = "d", Volume = "d", TowDirectionCode = "c",
-#                                            OrganismCode = "c", ForkLength = "d", Count = "d")))
-#
-# djfmp.noTowVol = DJFMP2[is.na(DJFMP2$Volume),]
-# temps = djfmp.noTowVol[!is.na(djfmp.noTowVol$StartMeter),]
-# temps = djfmp.noTowVol[(!is.na(djfmp.noTowVol$Volume) & is.na(djfmp.noTowVol$flowDebris)),]
-
-# Clean up; remove temporary files
-rm(DJFMP_stations)
-
-
