0af9e8c56e02ccc9b565b1100faacd586ab9c75d
lmitchell4 <lmitchell4@gmail.com>
2021-11-03 11:37:18 -0700

    Add STN raw data, code, tests, and saved data

---
+++ data-raw/STN.R
@@ -0,0 +1,218 @@
+
+## Data retrieval script for CDFW's Summer Townet Survey.
+## Database is too large to store on GitHub, so query now and save smaller csv files.
+
+#########################################################################################
+## Retrieve STN database copy, save tables, delete database.
+## Only run this section when needed.
+
+library(DBI)
+library(odbc)
+
+## STN database url and file names:
+dbName <- "STN_Data1959-2020.accdb"
+surveyURL <- paste0("https://filelib.wildlife.ca.gov/Public/TownetFallMidwaterTrawl",
+										"/TNS%20MS%20Access%20Data/TNS%20data/",dbName)
+tmpFile <- file.path(tempdir(), dbName)
+
+downloadCheck <- download.file(url=surveyURL, destfile=tmpFile, mode="wb")
+downloadCheck
+
+## Open connection to the database:
+dbString <- paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};",
+									 "Dbq=",tmpFile)
+con <- DBI::dbConnect(drv=odbc::odbc(), .connection_string=dbString)
+
+tables <- odbc::dbListTables(conn=con)
+tables
+
+## Save select tables:
+keepTables <- c("Catch","Length","luMicrocystis","luOrganism","luStation",
+								"luTide","luTowDirection","Sample","TowEffort",
+								"Web_Local_Meter_Corrections")
+for(tab in keepTables) {
+	tmp <- DBI::dbReadTable(con, tab)
+	write.csv(tmp, file=file.path("data-raw","STN",paste0(tab,".csv")), row.names=FALSE)
+}
+
+## Disconnect from database and remove original files:
+DBI::dbDisconnect(conn=con)
+unlink(tmpFile)
+
+
+#########################################################################################
+## Create and save compressed data files using raw tables.
+
+library(dplyr)
+library(lubridate)
+library(wql)
+require(LTMRdata)
+
+raw_data <- file.path("data-raw","STN")
+
+Catch <- read.csv(file.path(raw_data,"Catch.csv"), stringsAsFactors=FALSE)
+Length <- read.csv(file.path(raw_data,"Length.csv"), stringsAsFactors=FALSE)
+luMicrocystis <- read.csv(file.path(raw_data,"luMicrocystis.csv"), stringsAsFactors=FALSE)
+luOrganism <- read.csv(file.path(raw_data,"luOrganism.csv"), stringsAsFactors=FALSE)
+luStation <- read.csv(file.path(raw_data,"luStation.csv"), stringsAsFactors=FALSE)
+luTide <- read.csv(file.path(raw_data,"luTide.csv"), stringsAsFactors=FALSE)
+luTowDirection <- read.csv(file.path(raw_data,"luTowDirection.csv"), stringsAsFactors=FALSE)
+Sample <- read.csv(file.path(raw_data,"Sample.csv"), stringsAsFactors=FALSE)
+TowEffort <- read.csv(file.path(raw_data,"TowEffort.csv"), stringsAsFactors=FALSE)
+Web_Local_Meter_Corrections <- read.csv(file.path(raw_data,
+                                                  "Web_Local_Meter_Corrections.csv"),
+                                        stringsAsFactors=FALSE)
+
+
+suspect_fm_TowRowID <- c(6612,12815,2551,12786,8031,1857)
+# subset(TowEffort, TowRowID %in% suspect_fm_TowRowID)
+
+
+sampleSTN <- Sample %>%
+	dplyr::inner_join(TowEffort, by="SampleRowID") %>%
+	dplyr::left_join(luStation, by=c("StationCode"="StationCodeSTN")) %>%
+  dplyr::mutate(SampleDate=as.Date(SampleDate),
+                Year=lubridate::year(SampleDate)) %>%
+	dplyr::left_join(Web_Local_Meter_Corrections,
+	                 by=c("Year"="Study.Year","MeterSerial"="Meter.Serial")) %>%
+	dplyr::mutate(MeterTotal=ifelse((MeterOut - MeterIn) < 0,
+																	(MeterOut + 1000000 - MeterIn),
+																	(MeterOut - MeterIn)),
+								TowVolm3=ifelse(is.na(MeterIn), 735, k.factor*MeterTotal*1.49),
+								TowVolm3=ifelse(is.na(TowVolm3), 735, TowVolm3),
+								## Use CDFW default tow volume when comments indicate
+								## something went wrong with the flow meter or resulting
+								## volume is unusually small or large:
+								TowVolm3=ifelse(TowRowID %in% suspect_fm_TowRowID, 735,
+								                TowVolm3)) %>%
+  dplyr::arrange(SampleDate, Survey, StationCode, TowNumber) %>%
+  dplyr::mutate(Source="STN",
+                SampleID=paste(Source, 1:nrow(.)),
+                Method="STN Net",
+                TowTime=substring(TimeStart,12),
+                Datetime=paste(SampleDate, TowTime),
+                Date=parse_date_time(SampleDate, "%Y-%m-%d",
+                                     tz="America/Los_Angeles"),
+                Datetime=parse_date_time(if_else(is.na(TowTime), NA_character_,
+                                                 Datetime),
+                                         "%Y-%m-%d %%H:%M:%S",
+                                         tz="America/Los_Angeles"),
+                Depth=DepthBottom*0.3048, # Convert depth from feet to m
+                Cable_length=CableOut,    # in feet, based on CDFW's metadata
+                Temp_surf=TemperatureTop, # degrees Celsius
+                ## Secchi is already in cm.
+
+                ## Convert conductivity to salinity.
+                ## ConductivityTop is in micro-S/cm at 25 degrees Celsius.
+                ## Input for ec2pss should be in milli-S/cm.
+                Sal_surf=wql::ec2pss(ConductivityTop/1000, t=25),
+
+                Latitude=(LatD + LatM/60 + LatS/3600),
+                Longitude= -(LonD + LonM/60 + LonS/3600)) %>%
+  dplyr::left_join(luMicrocystis, by=c("Microcystis"="MicrocystisID")) %>%
+  dplyr::left_join(luTide, by=c("TideCode"="TideRowID")) %>%
+  dplyr::left_join(luTowDirection, by=c("TowDirection"="TowDirectionID")) %>%
+  dplyr::mutate(Tide=TideDesc,
+                Tow_direction=TowDirection.y,
+                TowNum=TowNumber,
+                Tow_volume=TowVolm3,
+                Station=StationCode) %>%
+  dplyr::select(TowRowID, Source, Station, Latitude, Longitude, Date, Datetime,
+                Survey, TowNum, Depth, SampleID, Method, Tide, Sal_surf,
+                Temp_surf, Secchi, Tow_volume, Tow_direction, Cable_length)
+
+
+fish_totalCatch <- Catch %>%
+  dplyr::filter(!is.na(Catch) & Catch > 0) %>%
+	dplyr::left_join(luOrganism, by=c("OrganismCode"="OrganismCodeSTN"))
+
+fish_lengthFreq_measured <- Length
+unique(fish_lengthFreq_measured$ForkLength)
+
+
+fish_adjustedCount <- fish_totalCatch %>%
+  dplyr::left_join(fish_lengthFreq_measured,
+									 by="CatchRowID") %>%
+  dplyr::group_by(TowRowID, CatchRowID, OrganismCode) %>%
+  dplyr::mutate(TotalMeasured=sum(LengthFrequency, na.rm=TRUE)) %>%
+  dplyr::ungroup() %>%
+	## Add total catch numbers:
+	## There are some cases where the number of fish measured is greater than the
+	## catch value in the Catch table. In these cases, use the number measured.
+	dplyr::mutate(CatchNew=ifelse(TotalMeasured > Catch, TotalMeasured, Catch)) %>%
+	## Calculate length-frequency-adjusted counts:
+  dplyr::mutate(Count=(LengthFrequency/TotalMeasured)*CatchNew) %>%
+  dplyr::left_join(Species %>% ## Add species names
+										dplyr::select(STN_Code, Taxa) %>%
+										filter(!is.na(STN_Code)),
+									 by=c("OrganismCode"="STN_Code"))
+
+
+## Examine the cases where number measured > catch:
+count_mismatch <- subset(fish_adjustedCount, CatchNew != Catch)
+nrow(count_mismatch)
+
+
+## Join sample and fish info:
+names(sampleSTN)
+names(fish_adjustedCount)
+intersect(names(sampleSTN), names(fish_adjustedCount))
+
+STN <- sampleSTN %>%
+	dplyr::left_join(fish_adjustedCount %>%
+										dplyr::select(TowRowID, OrganismCode, Taxa,
+										              ForkLength, LengthFrequency,
+										              Catch, CatchNew, Count),
+									 by="TowRowID") %>%
+	## Add reasoning for any NA lengths:
+  dplyr::mutate(Length_NA_flag=if_else(is.na(Catch), "No fish caught",
+                                      NA_character_),
+                Station=as.character(Station))
+
+
+## 0 lengths:
+index_0 <- which(STN$ForkLength == 0 & !is.na(STN$Taxa))
+STN[index_0, ]
+STN$ForkLength[index_0] <- NA
+STN$Length_NA_flag[index_0] <- "Unknown length"
+STN[index_0, ]
+
+
+## no lengths for calculating adjusted length frequencies (Count):
+## use the Catch value from Catch as Count and change Length_NA_flag.
+index_1 <- which(!is.na(STN$Catch) & is.na(STN$Count))
+STN[index_1, ]
+STN$Count[index_1] <- STN$Catch[index_1]
+STN$Length_NA_flag[index_1] <- "Unknown length"
+STN[index_1, ]
+
+
+## Check that all organism code values are represented in Species:
+all(STN$OrganismCode %in% Species$STN_Code)
+
+
+## Create final measured lengths data frame:
+STN_measured_lengths <- STN %>%
+	dplyr::select(SampleID, Taxa, ForkLength, LengthFrequency) %>%
+  dplyr::rename(Length=ForkLength,
+                Count=LengthFrequency)
+
+nrow(STN_measured_lengths)
+ncol(STN_measured_lengths)
+names(STN_measured_lengths)
+
+
+## Create final catch data frame:
+STN <- STN %>%
+  dplyr::rename(Length=ForkLength) %>%
+  dplyr::select(-TowRowID, -OrganismCode, -LengthFrequency, -Catch, -CatchNew)
+
+nrow(STN)
+ncol(STN)
+names(STN)
+
+
+## Save compressed data to /data:
+usethis::use_data(STN, STN_measured_lengths, overwrite=TRUE)
+
+
---
+++ tests/testthat/test-STN.R
@@ -0,0 +1,133 @@
+library(LTMRdata)
+
+
+#######################################################################################
+## STN
+
+test_that("Data dimensions are correct", {
+  expect_true(nrow(STN) == 182418)
+  expect_true(ncol(STN) == 22)
+
+	name_check <- c("Source","Station","Latitude","Longitude","Date","Datetime",
+									"Survey","TowNum","Depth","SampleID","Method","Tide","Sal_surf",
+									"Temp_surf","Secchi","Tow_volume","Tow_direction","Cable_length",
+									"Taxa","Length","Count","Length_NA_flag")
+  expect_true(all(names(STN) == name_check))
+})
+
+test_that("Source value is correct", {
+  expect_true(all(STN$Source == "STN"))
+})
+
+test_that("Station values are in the expected range", {
+	stations <- c(9,20,21,22,23,24,32,35,36,301,302,312,315,320,322,323,326,
+	              328,329,334,335,336,340,342,405,411,418,501,504,508,513,519,
+	              520,602,606,609,610,704,706,707,711,713,716,719,721,723,795,
+	              796,797,801,804,809,812,815,902,906,910,912,914,915,918,919)
+  expect_true(all(STN$Station %in% as.character(stations)))
+})
+
+test_that("Survey numbers are in the expected range", {
+	expect_true(all(STN$Survey %in% 1:6))
+})
+
+test_that("Tow numbers are in the expected range", {
+	expect_true(all(STN$TowNum %in% 1:4))
+})
+
+test_that("Depth values are in the expected range", {
+	expect_true(all( (STN$Depth > 0 & STN$Depth < 30) | is.na(STN$Depth) ))
+})
+
+test_that("Custom SampleID values are formatted as expected", {
+	expect_true(all(grepl("STN [0-9]+",STN$SampleID)))
+})
+
+test_that("Data are based on STN Net", {
+  expect_true(all(STN$Method == "STN Net"))
+})
+
+test_that("Temp_surf values are in the expected range", {
+  expect_true(all( (STN$Temp_surf > 0 & STN$Temp_surf < 40) |
+                     is.na(STN$Temp_surf) ))
+})
+
+test_that("Secchi values are in the expected range", {
+  expect_true(all( (STN$Secchi > 0 & STN$Secchi < 310) | is.na(STN$Secchi) ))
+})
+
+test_that("Tow_volume values are in the expected range", {
+  expect_true(all( (STN$Tow_volume > 0 & STN$Tow_volume < 20000) |
+                     is.na(STN$Tow_volume) ))
+})
+
+test_that("Cable_length values are in the expected range", {
+  expect_true(all( (STN$Cable_length > 0 & STN$Cable_length < 310) |
+										is.na(STN$Cable_length) ))
+})
+
+test_that("Length values are in the expected range", {
+  expect_true(all( (STN$Length >= 0 & STN$Length < 800) | is.na(STN$Length) ))
+})
+
+test_that("Count values are in the expected range", {
+  expect_true(all( (STN$Count >= 0 & STN$Count < 35000) | is.na(STN$Count) ))
+})
+
+test_that("Combinations of Taxa, Count, and Length_NA_flag are as expected", {
+	len_flag_values <- c("No fish caught","Unknown length")
+  expect_true(all(STN$Length_NA_flag %in% len_flag_values | is.na(STN$Length_NA_flag)))
+
+	## If Length_NA_flag is NA, Count and Taxa should be present:
+	sub_1 <- subset(STN, is.na(Length_NA_flag))
+  expect_true(sum(is.na(sub_1$Count)) == 0)
+  expect_true(sum(is.na(sub_1$Taxa)) == 0)
+
+	## If Count is present, Taxa should be present:
+	sub_2 <- subset(STN, !is.na(Count))
+  expect_true(sum(is.na(sub_2$Taxa)) == 0)
+
+	## If Count is present, check Length_NA_flag:
+	sub_3 <- subset(STN, !is.na(Count))
+  expect_true(all( (sub_3$Length_NA_flag %in% len_flag_values[1:2]) |
+											is.na(sub_3$Length_NA_flag) ))
+
+	## If Length_NA_flag is not missing, it should have one of the three values:
+	sub_4 <- subset(STN, !is.na(Length_NA_flag))
+	expect_true(all(sub_4$Length_NA_flag %in% len_flag_values))
+})
+
+test_that("Some adjusted fish counts are as expected", {
+	dsm <- subset(STN, Taxa == "Hypomesus transpacificus")
+	expect_true(nrow(dsm) == 15306)
+	expect_true(sum(dsm$Count) == 56273)
+
+	lfs <- subset(STN, Taxa == "Spirinchus thaleichthys")
+	expect_true(nrow(lfs) == 6423)
+	expect_true(sum(lfs$Count) == 17138)
+
+	chn <- subset(STN, Taxa == "Oncorhynchus tshawytscha")
+	expect_true(nrow(chn) == 145)
+	expect_true(sum(chn$Count) == 156)
+})
+
+
+#######################################################################################
+## STN_measured_lengths
+
+test_that("Lengths are in the expected range", {
+	expect_true(all( (STN_measured_lengths$Length > 0 & STN$Length < 900) |
+										is.na(STN_measured_lengths$Length) ))
+})
+
+test_that("Some total measured fish counts are as expected", {
+	dsm_len <- subset(STN_measured_lengths, Taxa == "Hypomesus transpacificus")
+	expect_true(sum(dsm_len$Count, na.rm=TRUE) == 56268)
+
+	lfs_len <- subset(STN_measured_lengths, Taxa == "Spirinchus thaleichthys")
+	expect_true(sum(lfs_len$Count, na.rm=TRUE) == 17138)
+
+	chn_len <- subset(STN_measured_lengths, Taxa == "Oncorhynchus tshawytscha")
+	expect_true(sum(chn_len$Count) == 156)
+})
+
