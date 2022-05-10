2d00d88ee189cbf053530b8c6a72888b150341c5
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2022-05-10 14:10:59 -0700

    Updating shiny app

--- inst/shiny/deltafish/app.R
+++ inst/shiny/deltafish/app.R
@@ -17,205 +17,256 @@ library(deltafish)
 create_fish_db()
 
 surv<-open_survey()%>%
-    mutate(StationID=paste(Source, Station),
-           Date=as_date(Date),
-           Year=as.integer(year(Date)),
-           Month=as.integer(month(Date)))
+  mutate(StationID=paste(Source, Station),
+         Date=as_date(Date),
+         Year=as.integer(year(Date)),
+         Month=as.integer(month(Date)))
 
 fish<-open_fish()
 
 surveys<-surv%>%
-    distinct(Source)%>%
-    collect()%>%
-    pull(Source)
-
-stations<-surv%>%
-    distinct(StationID)%>%
-    collect()%>%
-    as.vector()
+  distinct(Source)%>%
+  collect()%>%
+  pull(Source)
 
 years<-surv%>%
-    distinct(Year)%>%
-    collect()%>%
-    pull(Year)%>%
-    range()
+  distinct(Year)%>%
+  collect()%>%
+  pull(Year)%>%
+  range()
 
 species<-fish%>%
-    filter(Count>0)%>%
-    group_by(Taxa)%>%
-    summarise()%>%
-    collect()%>%
-    pull(Taxa)
+  distinct(Taxa)%>%
+  collect()%>%
+  pull(Taxa)%>%
+  sort()
 
 length_max<-fish%>%
-    filter(Length>0)%>%
-    select(Length)%>%
-    collect()%>%
-    pull(Length)%>%
-    max()
+  filter(Length>0)%>%
+  select(Length)%>%
+  collect()%>%
+  pull(Length)%>%
+  max()
+
+suisun_samples<-surv%>%
+  filter(Source=="Suisun")%>%
+  select(SampleID)%>%
+  collect()%>%
+  pull(SampleID)
 gc()
+
+#Settings for the "data crunching" message.
+info_loading <- "Crunching data"
+progress_color <- "black"
+progress_background <- "#c5c5c9"
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("deltafish"),
+  a(shiny::icon("reply"), "Delta Science shinyapps homepage", href="https://deltascience.shinyapps.io/Home/"),
+  # Application title
+  titlePanel("deltafish"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            pickerInput("Surveys",
-                        "Select surveys:",
-                        choices = surveys,
-                        multiple = TRUE,
-                        selected=surveys,
-                        options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3")),
-            pickerInput("Months",
-                        "Select months:",
-                        choices = 1:12,
-                        multiple = TRUE,
-                        selected=1:12,
-                        options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3")),
-            sliderInput("Years",
-                        "Select years:",
-                        min = years[1],
-                        max=years[2],
-                        value=years,
-                        step=1,
-                        sep=""),
-            pickerInput("Stations",
-                        "Select sampling stations:",
-                        choices = stations,
-                        multiple = TRUE,
-                        selected=stations,
-                        options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3")),
-            pickerInput("Species",
-                        "Select species:",
-                        choices = species,
-                        multiple = TRUE,
-                        selecte=species,
-                        options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3")),
-            prettySwitch("Aggregate",HTML("<b>Aggregate data?</b>"), status = "success", fill = TRUE, bigger=TRUE),
-            conditionalPanel(condition="input.Aggregate",
-                             sliderInput("Standardlength", "Standard length cutoff (mm; Suisun survey)", value = c(0, length_max), min=0, max=length_max, step = 1),
-                             sliderInput("Forklength", "Fork length cutoff (mm; all other surveys)", value = c(0, length_max), min=0, max=length_max, step = 1)),
-            h2("Rows:"),
-            textOutput("rows"),
-            h2("Estimated CSV size:"),
-            textOutput("size")
-        ),
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      pickerInput("Surveys",
+                  "Select surveys:",
+                  choices = surveys,
+                  multiple = TRUE,
+                  selected=surveys,
+                  options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3")),
+      pickerInput("Months",
+                  "Select months:",
+                  choices = 1:12,
+                  multiple = TRUE,
+                  selected=1:12,
+                  options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3")),
+      sliderInput("Years",
+                  "Select years:",
+                  min = years[1],
+                  max=years[2],
+                  value=years,
+                  step=1,
+                  sep=""),
+      pickerInput("Species",
+                  "Select species:",
+                  choices = species,
+                  multiple = TRUE,
+                  selecte=species,
+                  options=list(`actions-box`=TRUE, `selected-text-format` = "count > 3", `live-search`=TRUE)),
+      prettySwitch("Aggregate",HTML("<b>Sum counts over all lengths?</b>"), status = "success", fill = TRUE, bigger=TRUE),
+      conditionalPanel(condition="input.Aggregate",
+                       h4("Standard length cutoff (mm; Suisun survey)"),
+                       fluidRow(column(6, numericInput("Standardmin", "Min", value = 0, min=0, max=length_max, step = 1)),
+                                column(6, numericInput("Standardmax", "Max", value = length_max, min=0, max=length_max, step = 1))),
+                       h4("Fork length cutoff (mm; all other surveys)"),
+                       fluidRow(column(6, numericInput("Forkmin", "Min", value = 0, min=0, max=length_max, step = 1)),
+                                       column(6, numericInput("Forkmax", "Max", value = length_max, min=0, max=length_max, step = 1)))),
+      actionBttn("Run", "Run/Update", style="bordered", icon = icon("play"), color="danger", size="sm"),
+      h2("Rows:"),
+      textOutput("rows"),
+      h2("Can you safely open this in excel?"),
+      textOutput("excel"),
+      h2("Estimated CSV size:"),
+      textOutput("size")
+    ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("dataPlot")
-        )
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("dataPlot")
     )
+  ),
+  # Display the "data crunching" message.
+  tags$head(tags$style(type="text/css",
+                       paste0("
+                                             #loadmessage {
+                                             position: fixed;
+                                             top: 25%;
+                                             left: 25%;
+                                             width: 50%;
+                                             padding: 30px 0px 30px 0px;
+                                             text-align: center;
+                                             font-weight: bold;
+                                             font-size: 200%;
+                                             color: ", progress_color,";
+                                             background-color: ", progress_background,";
+                                             z-index: 105;
+                                             border: 2px solid black;
+                                             border-radius: 50px;
+                                             }
+                                             "))),
+  conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+                   tags$div(tags$i(class = "fa fa-spinner", style = "color: black"), info_loading, tags$i(class = "fa fa-spinner", style = "color: black"), id="loadmessage")),
+  tags$style(type="text/css", ".recalculating {opacity: 1.0;}")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    survey_filt<-reactive({
-        if(is.null(input$Surveys)){
-            surveys
-        }else{
-            input$Surveys
-        }
-    })
+  species_filt<-reactive({
+    if(is.null(input$Species)){
+      species
+    }else{
+      input$Species
+    }
+  })
 
-    month_filt<-reactive({
-        if(is.null(input$Month)){
-            1:12
-        }else{
-            input$Month
-        }
-    })
+  data_filt<-eventReactive(input$Run, {
+    req(species_filt)
 
-    year_filt<-reactive({
-        if(is.null(input$Years)){
-            years
-        }else{
-            input$Years
-        }
-    })
+    if(is.null(input$Surveys)){
+      survey_filt<-surveys
+    }else{
+      survey_filt<-input$Surveys
+    }
 
-    species_filt<-reactive({
-        if(is.null(input$Species)){
-            species
-        }else{
-            input$Species
-        }
-    })
+    if(is.null(input$Years)){
+      year_filt<-years
+    }else{
+      year_filt<-input$Years
+    }
 
-    data_filt<-reactive({
-        req(survey_filt, month_filt, year_filt, species_filt)
+    year_min<-min(year_filt)
+    year_max<-max(year_filt)
 
-        year_filts<-year_filt()
-        year_min<-min(year_filts)
-        year_max<-max(year_filts)
+    if(is.null(input$Month)){
+      month_filt<-1:12
+    }else{
+      month_filt<-input$Month
+    }
 
+    if(input$Aggregate){
+      req(input$Standardmin, input$Standardmax, input$Forkmin, input$Forkmax)
+    }
 
-        out<-surv%>%
-            filter(Source%in%local(survey_filt()) & Month%in%local(month_filt()) & Year>=year_min & Year<=year_max)%>%
-            left_join(fish%>%
-                          filter(Taxa%in%local(species_filt())),
-                      by="SampleID")
+    out<-surv%>%
+      {if(length(survey_filt)<length(surveys)){
+        filter(., Source%in%survey_filt)
+      }else{
+        .
+      }}%>%
+      {if(length(month_filt)<12){
+        filter(., Month%in%month_filt)
+      }else{
+        .
+      }}%>%
+      {if(year_min>min(years) | year_max<max(years)){
+        filter(., Year>=year_min & Year<=year_max)
+      }else{
+        .
+      }}%>%
+      left_join(fish%>%
+                  {if(length(species_filt())<length(species)){
+                    filter(., Taxa%in%local(species_filt()))
+                  }else{
+                    .
+                  }}%>%
+                  {if(input$Aggregate){
+                    filter(., (!SampleID%in%suisun_samples & Length>=local(input$Forkmin) & Length<=local(input$Forkmax)) |
+                             (SampleID%in%suisun_samples & Length>=local(input$Standardmin) & Length<=local(input$Standardmax)) |
+                             is.na(Length))%>%
+                      group_by(SampleID, Taxa)%>%
+                      summarise(Count=sum(Count, na.rm=T), .groups="drop")
+                  }else{
+                    .
+                  }},
+                by="SampleID")
+    return(out)
 
-        if(input$Aggregate){
-            req(input$Standardlength, input$Forklength)
-            standard_min<-min(input$Standardlength)
-            standard_max<-max(input$Standardlength)
-            fork_min<-min(input$Forklength)
-            fork_max<-max(input$Forklength)
-            out<-out%>%
-                filter((Source!="Suisun" & Length>=standard_min & Length<=standard_max) | (Source=="Suisun" & Length>=fork_min & Length<=fork_max) | is.na(Length))%>%
-                group_by(across(-c(Length, Count, Notes_catch)))%>%
-                summarise(Count=sum(Count, na.rm=T), .groups="drop")
+  })
 
-        }
-        return(out)
+  rows<-reactive({
+    req(data_filt)
+    data_filt()%>%
+      select(SampleID)%>%
+      collect()%>%
+      nrow
+  })
 
-    })
+  data_ag<-reactive({
+    req(isTRUE(input$Aggregate), input$Standardlength, input$Forklength)
 
-    rows<-reactive({
-        req(data_filt)
-        data_filt()%>%
-            select(SampleID)%>%
-            collect()%>%
-            nrow
-    })
+  })
 
-    data_ag<-reactive({
-        req(isTRUE(input$Aggregate), input$Standardlength, input$Forklength)
+  output$rows <- renderText({
+    req(rows)
+    format(rows(), big.mark=",")
+  })
 
-    })
+  output$excel <- renderText({
+    req(rows)
+    if(rows()>1048576){
+      "No"
+    }else{
+      "Yes"
+    }
+  })
 
-    output$rows <- renderText({
-        req(rows)
-        format(rows(), big.mark=",")
-    })
+  output$size <- renderText({
+    req(rows)
+    size<-rows()*0.00025*1048576 # estimated by writing csvs of different numbers of rows and extracting file size, then converting to bytes
+    class(size)<-"object_size"
+    return(format(size, units="auto"))
+  })
 
-    output$size <- renderText({
-        req(rows)
-        size<-rows()*0.00025*1048576 # estimated by writing csvs of different numbers of rows and extracting file size, then converting to bytes
-        class(size)<-"object_size"
-        return(format(size, units="auto"))
-    })
+  output$dataPlot <- renderPlot({
+    req(data_filt)
+    plot_data<-data_filt()%>%
+      select(Year, Taxa, Count)%>%
+      group_by(Year, Taxa)%>%
+      summarise(Count=sum(Count, na.rm=T), .groups="drop")%>%
+      collect()
 
-    output$dataPlot <- renderPlot({
-        plot_data<-data_filt()%>%
-            group_by(Year, Taxa)%>%
-            summarise(Count=sum(Count, na.rm=T), .groups="drop")%>%
-            collect()
+    p<-ggplot(plot_data, aes(x=Year, y=Count, fill=Taxa))+
+      geom_bar(position="stack", stat="identity")+
+      theme_bw()
 
-        p<-ggplot(plot_data, aes(x=Year, y=Count, fill=Taxa))+
-            geom_bar(position="stack", stat="identity")+
-            theme_bw()
-
-        if(length(species_filt())>10){
-            p<-p+theme(legend.position="none")
-        }
-        return(p)
-    })
+    if(length(unique(plot_data$Taxa))>10){
+      p<-p+theme(legend.position="none")
+    }
+    return(p)
+  })
 }
 
 # Run the application
