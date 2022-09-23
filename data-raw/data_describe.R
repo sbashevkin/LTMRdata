87d71accdb23bf3a18bb055726fe5605bb822b0a
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2022-09-23 16:13:03 -0700

    Created new graph for Lynn

--- data-raw/data_describe.R
+++ data-raw/data_describe.R
@@ -22,3 +22,14 @@ ggplot(fish_sum, aes(x=Taxa, y=Count, fill=Count))+
   theme_bw()+
   theme(axis.text.x = element_text(angle=90, vjust=0, hjust=1, face="italic", size=8), legend.position = "none", text=element_text(size=16))
 
+p<-ggplot(fish_sum, aes(x=Taxa, y=Count, fill=Count))+
+  geom_bar(stat="identity", color="white")+
+  geom_label_repel(data=fish_sum[seq(nrow(fish_sum), 88, by=-20),], aes(label=Taxa), direction="y", hjust=-0.5, fill="white", color="black", fontface="italic", size=5)+
+  scale_y_continuous(trans = "log", breaks=c(1, 10, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8), label=scales::label_number(big.mark = ","), expand=expansion(0, 0))+
+  ylab("Total count (log scale)")+
+  xlab("Species")+
+  scale_fill_viridis_c(trans = "log1p")+
+  theme_bw()+
+  theme(axis.text.x = element_blank(), legend.position = "none", text=element_text(size=24), panel.grid=element_blank(), axis.ticks.x=element_blank())
+
+ggsave(plot=p, filename="~/deltafish_plot.png", height=6, width=8, units="in", dpi=300)
