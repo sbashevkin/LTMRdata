680352bc25c6614fd3e04f35ba4f4ad0865e285a
Tham-E <enoch.tham@wildlife.ca.gov>
2023-01-03 15:08:57 -0800

    Update FMWT.R

--- data-raw/FMWT.R
+++ data-raw/FMWT.R
@@ -2,12 +2,15 @@
 require(readr)
 require(wql)
 require(dplyr)
+library(dbplyr)
 require(tidyr)
 require(lubridate)
 require(LTMRdata)
 require(stringr)
 require(RODBC)
-
+require(odbc)
+require(DBI)
+require(DBIsqldf)
 
 
 Path<-file.path(tempdir(), "MWT_data.zip")
@@ -25,7 +28,7 @@ db_path <- file.path(tempdir(),"MWT_data.accdb")
 
 # Just pastes driver info and file path together.
 full_path <- paste0(driver,"DBQ=",db_path)
-full_path
+
 # Connect to database using information above.
 conn <- odbcDriverConnect(full_path)
 names<-sqlTables(conn)
@@ -64,40 +67,9 @@ FMWT_Tables$Sample <- sqlFetch(conn, "Sample")%>%
   left_join(FMWT_Tables$Station, by="Station")%>% # Add station coordinates
   mutate(SampleID=1:nrow(.)) # Add unique identifier for each sample (net tow)
 
-
-sample_fmwt <- read_csv(file.path("data-raw", "FMWT", "Sample.csv"),
-                        col_types = cols_only(SampleRowID="i", StationCode="c", MethodCode="c", SampleDate="c",
-                                              SampleTimeStart="c", SurveyNumber="i", WaterTemperature="d",
-                                              Turbidity="d", Secchi="d", SecchiEstimated="l", ConductivityTop="d",
-                                              ConductivityBottom="d", TowDirectionCode="i", MeterStart="d",
-                                              MeterEnd="d", CableOut="d", TideCode="i", DepthBottom="d",
-                                              WeatherCode="i", Microcystis="i", WaveCode="i",
-                                              WindDirection="c", BottomTemperature="d"))%>%
-  rename(Station=StationCode, Method=MethodCode, Tide=TideCode, Time=SampleTimeStart, Depth=DepthBottom, Date=SampleDate)%>%
-  mutate(Date=parse_date_time(Date, "%m/%d/%Y %H:%M:%S", tz="America/Los_Angeles"),
-         Time = parse_date_time(Time, "%m/%d/%Y %H:%M:%S", tz="America/Los_Angeles"),
-         Tide=recode(Tide, `1` = "High Slack", `2` = "Ebb", `3` = "Low Slack", `4` = "Flood"), # Convert tide codes to values
-         Weather=recode(WeatherCode, `1` = "Cloud (0-33%)", `2` = "Cloud (33-66%)", `3` = "Cloud (66-100%)", `4` = "Rain"), # Convert weather codes to values
-         Waves=recode(WaveCode, `1` = "Calm", `2` = "Waves w/o whitecaps", `3` = "Waves w/ whitecaps"), # Convert wave codes to values
-         WindDirection=toupper(WindDirection), # Make Wind direction codes consistent
-         Datetime=parse_date_time(if_else(is.na(Time), NA_character_, paste0(Date, " ", hour(Time), ":", minute(Time))), "%Y-%m-%d %%H:%M", tz="America/Los_Angeles"),
-         Meter_total=MeterEnd-MeterStart)%>% # Calculate flowmeter total difference
-  mutate(Tow_volume = Meter_total*0.02687*10.7, # Calculate tow volume using formula provided by Steve Slater / James White
-         WindDirection=recode(WindDirection, "NA"=NA_character_, "N/A"=NA_character_))%>% # Convert NA codes to NA
-  dplyr::filter(Method=="MWTR")%>% # Select only Midwater Trawl. All rows are MWTR but just in case the data change
-  mutate(Method=recode(Method, MWTR="Midwater trawl"), # Recode method for consistency
-         Microcystis=recode(Microcystis, `1`="Absent", `2`="Low", `6`="Low", `3`="Medium", `4`="High", `5`="Very high"), #Convert Microcystis codes to values
-         Tow_direction = recode(TowDirectionCode, `1`="With current", `2`="Against current", `3`="Unknown"))%>% # Convert tow direction codes to values
-  select(-TowDirectionCode, -WeatherCode, -WaveCode, -MeterEnd, -MeterStart, -Meter_total)%>% # Remove unneeded variables
-  left_join(FMWT_Tables$Station, by="Station")%>% # Add station coordinates
-  mutate(SampleID=1:nrow(.)) # Add unique identifier for each sample (net tow)
-
-
 # Catch data --------------------------------------------------------------
-
-
-catch_fmwt <- read_csv(file.path("data-raw", "FMWT", "Catch.csv"),
-                       col_types = cols_only(CatchRowID="i", SampleRowID="i", OrganismCode="i", Catch="d"))%>%
+FMWT_Tables$Catch<- sqlFetch(conn, "Catch")%>%
+  select("CatchRowID","SampleRowID","OrganismCode","Catch")%>%
   dplyr::filter(!is.na(OrganismCode))%>% # Remove any records with an NA organism code
   left_join(Species%>% # Add species names
               select(OrganismCode=FMWT_Code, Taxa)%>%
@@ -107,28 +79,28 @@ catch_fmwt <- read_csv(file.path("data-raw", "FMWT", "Catch.csv"),
 
 
 # Length data -------------------------------------------------------------
-
-
-length_fmwt<- read_csv(file.path("data-raw", "FMWT", "Length.csv"), na=c("NA", "n/p", ""),
-                       col_types = cols_only(CatchRowID="i", ForkLength="d", LengthFrequency="d"))%>%
+FMWT_Tables$Length<- sqlFetch(conn, "Length")%>%
+  select("CatchRowID","ForkLength","LengthFrequency")%>%
   dplyr::filter(ForkLength!=0)%>% # 0 fork length means not measured, so removing those from length table so those fish can be redistributed among measured lengths
   group_by(CatchRowID, ForkLength)%>%
-  summarise(LengthFrequency=sum(LengthFrequency), .groups="drop")
+  summarise(LengthFrequency=sum(LengthFrequency), .groups="drop")%>%
+  as.data.frame()
 
-catchlength_fmwt <- catch_fmwt%>%
-  left_join(length_fmwt%>%
+
+FMWT_Tables$CatchLength<- FMWT_Tables$Catch%>%
+  left_join(FMWT_Tables$Length%>%
               group_by(CatchRowID)%>%
               mutate(TotalMeasured=sum(LengthFrequency, na.rm=T))%>% # Calculate total number of fish measured for each species in each sample
               ungroup(),
             by="CatchRowID")%>% # Add catch numbers and species names
-  mutate(Count = if_else(is.na(ForkLength), Catch, (LengthFrequency/TotalMeasured)*Catch))%>% # Calculate adjusted count
+  mutate(Count = ifelse(is.na(ForkLength), Catch, (LengthFrequency/TotalMeasured)*Catch))%>% # Calculate adjusted count
   dplyr::filter(!is.na(Count))
 
 # Create final datasets ---------------------------------------------------
 
 
-FMWT<-sample_fmwt%>% # Start with sample to ensure samples without any catch (empty nets) are included
-  left_join(catchlength_fmwt, by="SampleRowID")%>% # Join to catch/length data
+FMWT<-FMWT_Tables$Sample%>% # Start with sample to ensure samples without any catch (empty nets) are included
+  left_join(FMWT_Tables$CatchLength, by="SampleRowID")%>% # Join to catch/length data
   mutate(Sal_surf=ec2pss(ConductivityTop/1000, t=25), # Convert conductivity to salinity
          Sal_bott=ec2pss(ConductivityBottom/1000, t=25),
          Secchi=Secchi*100, # Convert Secchi to cm from m
@@ -154,7 +126,7 @@ FMWT<-sample_fmwt%>% # Start with sample to ensure samples without any catch (em
 
 # Just measured lengths
 
-FMWT_measured_lengths<-length_fmwt%>%
+FMWT_measured_lengths<-FMWT_Tables$Length%>%
   left_join(FMWT%>% # Join species names and sampleID
               select(CatchRowID, SampleID, Taxa)%>%
               distinct(),
@@ -169,6 +141,6 @@ FMWT<-FMWT%>%
   group_by(across(-Count))%>% # Add up any new multiples after removing lifestages
   summarise(Count=sum(Count), .groups="drop")
 
-rm(catchlength_fmwt, catch_fmwt, length_fmwt, sample_fmwt, stations_fmwt) # Clean up
+rm(FMWT_Tables) # Clean up
 
 usethis::use_data(FMWT, FMWT_measured_lengths, overwrite=TRUE, compress="xz") # Save compressed data to /data
