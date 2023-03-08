7085deae626645e4637c12dc236e499ef8a612e7
Tham-E <enoch.tham@wildlife.ca.gov>
2023-03-08 12:58:58 -0800

    Update Salvage.R and Salvage.rda
    - create 1:1 query from standalone database

--- data-raw/Salvage.R
+++ data-raw/Salvage.R
@@ -34,32 +34,14 @@ SalvageTables <- bridgeAccess(db_path,
 # setting up the combined table to link with correct ID"s
 
 options(timeout = 9999)
-Salvage<-left_join(SalvageTables$Catch,SalvageTables$OrganismsLookUp[c("OrganismCode","CommonName","Genus","Species")],by="OrganismCode")%>%
+Salvage<-left_join(SalvageTables$Sample,SalvageTables$Building,by="SampleRowID",multiple="all")%>%
+  left_join(SalvageTables$Catch,by="BuildingRowID",multiple="all")%>%
   left_join(SalvageTables$Length,by="CatchRowID",multiple="all")%>%
-  left_join(SalvageTables$Building,by="BuildingRowID")%>%
-  left_join(SalvageTables$Sample,by="SampleRowID")%>%
-  left_join(SalvageTables$StudiesLookUp,by="StudyRowID")%>%
-  dplyr::select(SampleDate,SampleTime,SampleMethod,BuildingCode,
-                PrimaryDepth,PrimaryFlow,SampleRowID,BuildingRowID,CatchRowID,
-                AcreFeet, MinutesPumping,SampleTimeLength,StudyRowID,WaterTemperature,
-                OrganismCode,CommonName,Genus,Species,
-                ForkLength,LengthFrequency,AdiposeClip,Count,
-                BayPump1,BayPump2,BayPump3,BayPump4,BayPump5)%>%
-  #dplyr::rename(SampleDuration=SampleTimeLength)%>%
-  mutate(Taxa=paste(Genus,Species,sep=" "))%>%
-  dplyr::select(SampleDate,StudyRowID,MinutesPumping,SampleTime,
+  left_join(SalvageTables$OrganismsLookUp, by="OrganismCode",multiple="all")%>%
+  dplyr::select(SampleDate,StudyRowID,MinutesPumping,SampleTimeLength,
                 PrimaryDepth,PrimaryFlow,WaterTemperature,SampleRowID,
-                BuildingRowID,OrganismCode,Count,CommonName,SampleTimeLength,
+                BuildingRowID,OrganismCode,Count,CommonName,
                 BuildingCode,CatchRowID,ForkLength,LengthFrequency,
-                BayPump1,BayPump2,BayPump3,BayPump4,BayPump5)%>%
-  dplyr::mutate(ExpansionFactor=ifelse(StudyRowID=="0000",as.numeric(MinutesPumping/SampleTimeLength),
-                                       ifelse(StudyRowID=="9999",1,as.numeric(NA))),
-                ExpandedSalvage=LengthFrequency*ExpansionFactor,
-                Year=lubridate::year(SampleDate),
-                SampleDate=as.Date(SampleDate),
-                #CalDayStart = as.Date(paste(Year,"01-01",sep="-")),
-                #CalDay = as.numeric(difftime(SampleDate,CalDayStart,units="days")),
-                #WYear= ifelse(SampleDate<as.Date(paste(Year,"10-01",sep="-")),Year,Year+1)
-  )
+                BayPump1,BayPump2,BayPump3,BayPump4,BayPump5)
 
 usethis::use_data(Salvage, overwrite=TRUE)
