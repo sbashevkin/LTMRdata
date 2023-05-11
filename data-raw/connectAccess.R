0da6a52bbf41ad8d14d91beb029cf613aeaf03eb
Trinh Nguyen <49252874+trinhxuann@users.noreply.github.com>
2023-05-11 09:27:56 -0700

    Updating species table
    Removing period from Temp. to pass testthat.
    
    Arrow package testthat is causing slight issues due to the size of the data package.

--- data-raw/connectAccess.R
+++ data-raw/connectAccess.R
@@ -2,14 +2,14 @@ connectAccess <- function(file,
                           driver = "Microsoft Access Driver (*.mdb, *.accdb)", uid = "", pwd = "", ...) {
 
   file <- normalizePath(file, winslash = "\\")
-  
+
   # Driver and path required to connect from RStudio to Access
   dbString <- paste0("Driver={", driver,
                 "};Dbq=", file,
                 ";Uid=", uid,
-                ";Pwd=", pwd, 
+                ";Pwd=", pwd,
                 ";")
-  
+
   tryCatch(DBI::dbConnect(drv = odbc::odbc(), .connection_string = dbString),
                   error = function(cond) {
                     if (all(stringr::str_detect(cond$message, c("IM002", "ODBC Driver Manager")))) {
@@ -23,7 +23,7 @@ connectAccess <- function(file,
 }
 
 extractTables <- function(con, tables, out) {
-  
+
   # Pulling just the table names
   # tableNames <- RODBC::sqlTables(con, tableType = c("TABLE", "VIEW"))["TABLE_NAME"]
   tableNames <- odbc::dbListTables(conn = con)
@@ -32,15 +32,15 @@ extractTables <- function(con, tables, out) {
   # tableNames <- stringr::str_subset(tableNames, "MSys", negate = T)
   if (length(tables) == 1 & all(tables %in% "check")) {
     # If no table names are specified, then simply return the names of the possible databases for the user to pic
-    
+
     # RODBC::odbcClose(con)
     DBI::dbDisconnect(con)
-    
+
     cat("Specify at least one table to pull from: \n")
-    
+
     return(print(tableNames))
   }
- 
+
   # Apply the dbReadTable to each readable table in db
   # returnedTables <- mapply(RODBC::sqlQuery,
   #                          query = paste("SELECT * FROM", tables),
@@ -48,15 +48,15 @@ extractTables <- function(con, tables, out) {
   #                          SIMPLIFY = F)
   returnedTables <- mapply(DBI::dbReadTable,
                        name = tables,
-                       MoreArgs = list(conn = con), 
+                       MoreArgs = list(conn = con),
                        SIMPLIFY = F)
-  
+
   # names(returnedTables) <- tables
-  
+
   DBI::dbDisconnect(con)
   # RODBC::odbcClose(con)
-  
-  if (length(tables) != 1 & all(tables %in% "check")) {
+
+  if (rBit == "x64" & officeBit == "x32") {
     # Save the table to be read back into R
     saveRDS(returnedTables, file = file.path(out, "savedAccessTables.rds"))
   } else {
@@ -69,13 +69,13 @@ extractTables <- function(con, tables, out) {
 # Pull out arguments from the command line for use in the following functions
 if (!exists("Args")) {
   Args <- commandArgs(T)
-  
+
   file <- Args[1]
   out <- Args[2]
   tables <- Args[3:length(Args)]
-  
+
   con <- connectAccess(file)
-  
+
   extractTables(con = con,
                 tables = tables,
                 out = out)
